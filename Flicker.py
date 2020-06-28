7cfbed7fb2153dad837c1a4315c2f186637c1119
hishamsherief <hishamsherief1@gmail.com>
2020-06-28 16:48:25 +0530

    python3

---
+++ Flicker.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import webbrowser
+
+# apply for a flickr authentication key at http://www.flickr.com/services/apps/create/apply/?
+# paste the key (not the secret) as the value of the variable flickr_key
+
+flickr_key = 
+
+def get_flickr_data(tags_string):
+    baseurl = "https://api.flickr.com/services/rest/"
+    params_diction = {}
+    params_diction["api_key"] = flickr_key
+    params_diction["tags"] = tags_string
+    params_diction["tag_mode"] = "all"
+    params_diction["method"] = "flickr.photos.search"
+    params_diction["per_page"] = 5
+    params_diction["media"] = "photos"
+    params_diction["format"] = "json"
+    params_diction["nojsoncallback"] = 1
+    flickr_resp = requests.get(baseurl, params = params_diction)
+    #print(flickr_resp.url)
+    return flickr_resp.json()
+
+result_river_mts = get_flickr_data("river,mountains")
+
+photos = result_river_mts['photos']['photo']
+
+#To open images in webbrowser
+for photo in photos:
+    owner = photo['owner']
+    photo_id = photo['id']
+    url = 'https://www.flickr.com/photos/{}/{}'.format(owner, photo_id)
+    webbrowser.open(url)
